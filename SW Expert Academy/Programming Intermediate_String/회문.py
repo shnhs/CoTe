ad39096c74eb0cb88f49736f1b179080b9df04ef
shnhs <93372001+shnhs@users.noreply.github.com>
2022-01-17 21:02:19 +0900

    회문 문제 커밋
    회문 문제 풀어서 커밋, input.txt랑 temp 파일 삭제

--- SW Expert Academy/Programming Intermediate_String/회문.py
+++ SW Expert Academy/Programming Intermediate_String/회문.py
@@ -1,7 +1,3 @@
-from math import ceil
-from tkinter import font
-
-
 n = open("SW Expert Academy\Programming Intermediate_String\input.txt", "r")
 
 T = int(n.readline())
@@ -20,18 +16,70 @@ for test_case in range(1, T + 1):
     # 가로 탐색
     for i in range(N):
         for j in range(N):
+            # M(회문길이) 홀수일때
             if M % 2 == 1:
+                front_part = ''.join(reversed(box[i][j:j+int(M/2)])) # 회문 탐색 앞부분(미리 뒤집는다)
+                back_part = ''.join(box[i][j+1+int(M/2):j+1+2*int(M/2)]) # 회문 탐색 뒷부분
+                if front_part == back_part: # 회문이라면
+                    ans = ''.join(box[i][j:j+M]) # 처음인덱스~마지막인덱스 문자열 출력
+            # M(회문길이) 짝수일때
+            else:
                 front_part = ''.join(reversed(box[i][j:j+int(M/2)]))
-                back_part = ''.join(box[i][j+1+int(M/2):j+1+2*int(M/2)])
+                back_part = ''.join(box[i][j+int(M/2):j+2*int(M/2)])
                 if front_part == back_part:
-                    ans = box[i][j:j+M]
+                    ans = ''.join(box[i][j:j+M])
+
+    # 세로탐색
+    for i in range(N):
+        for j in range(N):
+            # M(회문길이) 홀수일때
+            if M%2 == 1:
+                front_temp = []
+                # 세로로 회문 앞부분을 탐색하여 리스트로 만들고 문자열로 저장
+                for x in range(int(M/2)):
+                    try:
+                        front_temp.append(box[j+x][i])
+                    except:
+                        pass
+                # 뒤집어서 저장
+                front_part = ''.join(reversed(front_temp))
+
+                # 세로로 회문 뒷부분을 탐색하여 리스트로 만들고 문자열로 저장
+                back_temp = []
+                for x in range(int(M/2)+1,2*int(M/2)+1):
+                    try:
+                        back_temp.append(box[j+x][i])
+                    except:
+                        pass
+                back_part = "".join(back_temp)
                 
+                # 회문이라면 세로로 문자열 출력
+                ans_temp = []
+                if front_part == back_part:
+                    for x in range(2*int(M/2)+1):
+                        ans_temp.append(box[j+x][i])
+                    ans = ''.join(ans_temp)
+            else:
+                # M(회문길이) 짝수일때
+                front_temp = []
+                for x in range(int(M/2)):
+                    try:
+                        front_temp.append(box[j+x][i])
+                    except:
+                        pass
+                front_part = ''.join(reversed(front_temp))
+                back_temp = []
+                for x in range(int(M/2),2*int(M/2)):
+                    try:
+                        back_temp.append(box[j+x][i])
+                    except:
+                        pass
+                back_part = "".join(back_temp)
 
-    # # 세로 탐색
-    # for i in range(N):
-    #     for j in range(N):
-    #         print('')
+                ans_temp = []
+                if front_part == back_part:
+                    for x in range(2*int(M/2)):
+                        ans_temp.append(box[j+x][i])
+                    ans = ''.join(ans_temp)
 
-    print(box)
-
-    # print(f'#{test_case} {ans}')
+    print(f'#{test_case} {ans}')
